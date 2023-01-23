37f584a0daed068793e27ea103de9531b87c3ea0
DinoCastagno21 <108820665+DinoCastagno21@users.noreply.github.com>
2023-01-23 11:05:18 -0300

    delete

--- proyecto final/README.md
+++
@@ -1,3 +0,0 @@
-# Proyecto_Final
-
-Pàgina de curriculum vitae, demostrando lo aprendido en el curso de Programacion: Primeros pasos del desarrollo frontend.
--- proyecto final/funtion.js
+++
@@ -1,8 +0,0 @@
- function fotoCompleta(){
-    
-    let img = document.getElementById('foto')
-     img.style.visibility = 'visible';
-     img.onclick = () => {
-        img.style.visibility = 'hidden';
-     }
-}
